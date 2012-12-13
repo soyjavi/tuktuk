7a6b045372f3e66703a4c0527579c90b514ef817
soyjavi <javi.jimenez.villar@gmail.com>
2012-12-13 11:00:57 +0100

    New BUILD.sh

--- vendor/build.sh
+++ vendor/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 COMPRESSOR=yuicompressor/yuicompressor-2.4.2.jar
-SOURCES=../
+STYLESHEETS=../stylesheets/
+WIDGETS=../widgets/
 NAMESPACE=tuktuk.
+NAMESPACE_WIDGET=tuktuk.widget.
+
 PACKAGE=../package/
+PACKAGE_WIDGETS=../package/widgets/
+
+
 MINIFIED="min"
 
 #script
@@ -10,7 +16,7 @@ clear
 echo -e "\033[0m"============================ LUNGO COMPILER ============================
 
     FILES_TO_COMPRESS=""
-    DIR=$SOURCES"stylesheets/css/"
+    DIR=$STYLESHEETS"css/"
 
     echo -e "\033[33m  [DIR]: "$DIR" >> COMPRESSING"
     FILES=(layout.css grid.css flexbox.css font.css form.css table.css button.css nav.css list.css widget.css media.css)
@@ -18,13 +24,12 @@ echo -e "\033[0m"============================ LUNGO COMPILER ===================
     do
         FILES_TO_COMPRESS=$FILES_TO_COMPRESS" "$DIR$NAMESPACE$file
     done
-    cat $FILES_TO_COMPRESS > $PACKAGE/$NAMESPACE"temp.css"
-    java -jar $COMPRESSOR $PACKAGE/$NAMESPACE"temp.css" -o $PACKAGE/$NAMESPACE"css"
-    rm $PACKAGE/$NAMESPACE"temp.css"
+    cat $FILES_TO_COMPRESS > $PACKAGE/$NAMESPACE"css"
+    java -jar $COMPRESSOR $PACKAGE/$NAMESPACE"css" -o $PACKAGE/$NAMESPACE"min.css"
 
     echo -e "\033[32m    [BUILD]: tuktuk.css\033[0m"
 
-    DIR=$SOURCES"stylesheets/"
+    DIR=$STYLESHEETS
     FILES=(tuktuk.theme.styl __vendor.styl)
     for file in "${FILES[@]}"
     do
@@ -32,7 +37,19 @@ echo -e "\033[0m"============================ LUNGO COMPILER ===================
         cp $DIR$file $PACKAGE$file
     done
     cp $DIR"css/tuktuk.theme.css" $PACKAGE
-    cp $DIR"css/tuktuk.icon.css" $PACKAGE
+
+    # WIDGETS
+    # Icon
+    cp $WIDGETS"icon/css/"$NAMESPACE_WIDGET"icon.css" $PACKAGE_WIDGETS
+
+    # Modal
+    cp $WIDGETS"modal/css/"$NAMESPACE_WIDGET"modal.css" $PACKAGE_WIDGETS
+    cp $WIDGETS"modal/build/"$NAMESPACE_WIDGET"modal.js" $PACKAGE_WIDGETS
+
+
+
+
+
 
 
 echo ============================ /LUNGO COMPILER ============================
