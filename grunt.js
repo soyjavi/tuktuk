de6cdc70942c528b829eebcd7666f275705a9ea6
soyjavi <javi.jimenez.villar@gmail.com>
2012-12-28 11:29:46 +0100

    GruntJS task

---
+++ grunt.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package/component.json>',
+
+    meta: {
+        file: "tuktuk",
+        banner: '/* <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy/m/d") %>\n' +
+                '   <%= pkg.homepage %>\n' +
+                '   Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>' +
+                ' - Licensed <%= _.pluck(pkg.license, "type").join(", ") %> */'
+    },
+
+    resources: {
+        coffee: ['**/*.coffee'],
+        stylus: ['**/*.styl'],
+
+        stylesheets: ['stylesheets/tuktuk.*.styl'],
+        themes: ['stylesheets/theme.*.styl'],
+        widgets: ['widgets/**/*.styl']
+    },
+
+    stylus: {
+      stylesheets: {
+        options: { compress: true },
+        files: { 'package/<%=meta.file%>.css': '<config:resources.stylesheets>' }
+      },
+      themes: {
+        options: { compress: false },
+        files: { 'package/*.css': '<config:resources.themes>' }
+      },
+      widgets: {
+        options: { compress: true },
+        files: { 'package/widgets/*.css': '<config:resources.widgets>' }
+      }
+    },
+
+    coffee: {
+      app: {
+        src: ['<config:resources.coffee>'],
+        dest: 'package',
+        options: {
+            bare: true,
+            preserve_dirs: true
+        }
+      }
+    },
+
+    watch: {
+      files: ['<config:resources.stylus>', '<config:resources.coffee>'],
+      tasks:  'stylus coffee'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-coffee');
+
+  // Default task.
+  grunt.registerTask('default', 'stylus coffee');
+
+};
