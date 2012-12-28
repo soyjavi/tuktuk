f00fa7692f7fa5a109a879a3c7d5c6d976c4481c
soyjavi <javi.jimenez.villar@gmail.com>
2012-12-28 11:29:46 +0100

    GruntJS task

---
+++ package/widgets/modal/tuktuk.widget.modal.js
@@ -0,0 +1,55 @@
+var TukTuk,
+  __slice = [].slice;
+
+TukTuk = {
+  dom: function() {
+    var args;
+    args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+    if (typeof $$ !== "undefined" && $$ !== null) {
+      return $$.apply(null, args);
+    } else {
+      return $.apply(null, args);
+    }
+  }
+};
+
+window.TukTuk = TukTuk;
+
+TukTuk.Modal = (function(tk, undefined_) {
+  var hide, lock, modal, show;
+  lock = void 0;
+  modal = void 0;
+  /*
+        @todo: Describe method
+  */
+
+  show = function(modal_id) {
+    modal = tk.dom("[data-tuktuk=modal]#" + modal_id).first();
+    modal.addClass("active");
+    lock.addClass("active");
+    return this;
+  };
+  /*
+        @todo: Describe method
+  */
+
+  hide = function() {
+    lock.removeClass("active");
+    modal.removeClass("active");
+    return this;
+  };
+  return {
+    _Instance: (function() {
+      tk.dom("[data-tuktuk=modal] [data-action=close]").on("click", function() {
+        return TukTuk.Modal.hide();
+      });
+      tk.dom("[data-tuktuk-modal]").on("click", function() {
+        return TukTuk.Modal.show(tk.dom(this).attr('data-tuktuk-modal'));
+      });
+      tk.dom(document.body).append("<div data-tuktuk=\"lock\"></div>");
+      return lock = tk.dom("[data-tuktuk=lock]").first();
+    })(),
+    show: show,
+    hide: hide
+  };
+})(TukTuk);
